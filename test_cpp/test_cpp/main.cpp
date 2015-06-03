aaca3b6ba3114ed3ad0c2eebff46710cf5477ccb
Pablo coco <coen11@naver.com>
2015-06-03 10:07:09 +0900

    add cout function

--- test_cpp/test_cpp/main.cpp
+++ test_cpp/test_cpp/main.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    std::cout << "kkk pablococo\n";
     std::cout << "Hello, World!\n";
     return 0;
 }
