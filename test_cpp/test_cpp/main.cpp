898c358c64b530394adb8a020f4d4da90f82d54e
Pablo coco <coen11@naver.com>
2015-06-05 11:12:48 +0900

    일상적인 업무 1 추가

--- test_cpp/test_cpp/main.cpp
+++ test_cpp/test_cpp/main.cpp
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     std::cout << "kkk pablococo\n";
     std::cout << "방배동 레스토랑\n";
     std::cout << "Hello, World!\n";
+    std::cout << "일상적인 업무\n";
     return 0;
 }
